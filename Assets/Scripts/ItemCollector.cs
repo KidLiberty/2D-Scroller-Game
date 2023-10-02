b4645df8f8c7ea8c6af217136413748f893e99ab
Brandon Stone <brandonrstone@gmail.com>
2023-10-02 10:55:06 -0400

    Initial commit

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+
+    private int kiwis = 0;
+
+    [SerializeField] private Text kiwisText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Kiwi"))
+        {
+            Destroy(collision.gameObject); // ref to Kiwi
+            kiwis++;
+            kiwisText.text = "Kiwis: " + kiwis;
+        }
+    }
+
+}
